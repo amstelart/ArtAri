40816808feeec67f28ec36cb73d7a7da1fd4db37
amstelart <amsteldroid@gmail.com>
2020-08-17 05:48:23 +0300

    dev2

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -3,6 +3,96 @@ $( document ).ready(function() {
 
   $('select').niceSelect();
 
+  $(".product-rating").starRating({
+    starSize: 40,
+    readOnly: true,
+    strokeWidth: 0,
+    useGradient: false,
+    callback: function(currentRating, $el){
+        // make a server call here
+    }
+  });
+  //
+  // product-carousel
+  var sync1 = $("#product-view");
+  var sync2 = $("#product-thumbnails");
+  var slidesPerPage = 4; //globaly define number of elements per page
+  var syncedSecondary = true;
+
+  sync1.owlCarousel({
+    items : 1,
+    slideSpeed : 2000,
+    nav: false,
+    autoplay: false,
+    dots: true,
+    loop: true,
+    responsiveRefreshRate : 200,
+    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>','<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
+  }).on('changed.owl.carousel', syncPosition);
+
+  sync2
+    .on('initialized.owl.carousel', function () {
+      sync2.find(".owl-item").eq(0).addClass("current");
+    })
+    .owlCarousel({
+    items : slidesPerPage,
+    dots: true,
+    nav: true,
+    smartSpeed: 200,
+    slideSpeed : 500,
+    slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+    responsiveRefreshRate : 100
+  }).on('changed.owl.carousel', syncPosition2);
+
+  function syncPosition(el) {
+    //if you set loop to false, you have to restore this next line
+    //var current = el.item.index;
+    
+    //if you disable loop you have to comment this block
+    var count = el.item.count-1;
+    var current = Math.round(el.item.index - (el.item.count/2) - .5);
+    
+    if(current < 0) {
+      current = count;
+    }
+    if(current > count) {
+      current = 0;
+    }
+    
+    //end block
+
+    sync2
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = sync2.find('.owl-item.active').length - 1;
+    var start = sync2.find('.owl-item.active').first().index();
+    var end = sync2.find('.owl-item.active').last().index();
+    
+    if (current > end) {
+      sync2.data('owl.carousel').to(current, 100, true);
+    }
+    if (current < start) {
+      sync2.data('owl.carousel').to(current - onscreen, 100, true);
+    }
+  }
+  
+  function syncPosition2(el) {
+    if(syncedSecondary) {
+      var number = el.item.index;
+      sync1.data('owl.carousel').to(number, 100, true);
+    }
+  }
+  
+  sync2.on("click", ".owl-item", function(e){
+    e.preventDefault();
+    var number = $(this).index();
+    sync1.data('owl.carousel').to(number, 300, true);
+  });
+  // end product-carousel
+  //
+
   $("#owl-carousel-demo").owlCarousel({
     items: 3,
     nav: false,
@@ -20,13 +110,13 @@ $( document ).ready(function() {
         items: 3,
       },
       768 : {
-        items: 3,
+        items: 4,
       },
       992 : {
-        items: 3,
+        items: 7,
       },
       1200 : {
-        items: 6,
+        items: 7,
       },
       1800 : {
         items: 7,
@@ -53,7 +143,7 @@ $( document ).ready(function() {
         items: 3,
       },
       992 : {
-        items: 3,
+        items: 4,
       },
       1200 : {
         items: 4,
@@ -77,13 +167,13 @@ $( document ).ready(function() {
         center: true,
       },
       480 : {
-        items: 3,
+        items: 1,
       },
       768 : {
-        items: 3,
+        items: 1,
       },
       992 : {
-        items: 3,
+        items: 4,
       },
       1200 : {
         items: 4,
@@ -174,13 +264,13 @@ $( document ).ready(function() {
 // }());
 
 // На проекте нет jQuery, но хочется $( document ).ready...
-// function ready(fn) {
-//   if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
-//     fn();
-//   } else {
-//     document.addEventListener('DOMContentLoaded', fn);
-//   }
-// }
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
 //
 // ready(function(){
 //   // code
